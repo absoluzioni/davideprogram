6baa32204f5aff836d75327200734994971f7ef3
Andrea <absoluzioni@gmail.com>
2023-12-28 17:03:57 +0100

    correct the distance between h1 and his underline

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 let titolo = document.querySelector("h6");
 let pagina = document.querySelector("body");
-let myHTML = `
-    <div id="fumetto">
-        <img src="./img/schiappa.png">
-        <p> Sei<br/>proprio<br/>una<br/>schiappa!!! </p>
-    </div>
-`;
+let fumetto = document.querySelector('#fumetto');
+
+fumetto.addEventListener("click", function() {
+    fumetto.classList.add("display-none");
+});
+
 titolo.addEventListener("click", function() {
-    let fumetto = document.querySelector('#fumetto');
-    if (!fumetto) {
-        pagina.insertAdjacentHTML("beforeend", myHTML);
-        setTimeout(function() {
-            let fumetto = document.querySelector('#fumetto');
-            fumetto.addEventListener("click", function() {
-                fumetto.remove();
-            })
-        }, 100)
-    }
+    fumetto.classList.remove("display-none");
 });
