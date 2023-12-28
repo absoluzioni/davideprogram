bb2ee65eb26f0c1ec932ba634334f7d237779c75
Andrea <absoluzioni@gmail.com>
2023-12-28 16:10:48 +0100

    tolto testo all'img e aggiunto testo personalizzato

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let titolo = document.querySelector("h6");
 let pagina = document.querySelector("body");
-
+let myHTML = `
+    <div id="fumetto">
+        <img src="./img/schiappa.png">
+        <p> Sei<br/>proprio<br/>una<br/>schiappa!!! </p>
+    </div>
+`;
 titolo.addEventListener("click", function() {
-    pagina.insertAdjacentHTML("beforeend", `
-        <div style="position: absolute;top: 50%; left: 50%;">
-            <img src="./img/schiappa.png" style="height: 200px;">
-            <p style="position: absolute;top: 1%;margin: 0;">Sei proprio una schiappa!!!</p>
-        </div>
-    `);
+    let fumetto = document.querySelector('#fumetto');
+    if (!fumetto) {
+        pagina.insertAdjacentHTML("beforeend", myHTML);
+        setTimeout(function() {
+            let fumetto = document.querySelector('#fumetto');
+            fumetto.addEventListener("click", function() {
+                fumetto.remove();
+            })
+        }, 100)
+    }
 });
